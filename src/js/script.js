5b3c4f6ed9f6238b12ed7d31547faa707628bb3e
AntonioPROLucas <106645196+AntonioPROLucas@users.noreply.github.com>
2024-05-16 20:58:49 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@
   //definir constantes e funções
   const btnMostrarMais = document.querySelector(".btn-mostrar-projetos");
   const projetosInativos = document.querySelectorAll(".projeto:not(.ativo)");
+  const btnMostrarCertificados = document.querySelector(".btn-mostrar-certificados");
+  const certificadosInativos = document.querySelectorAll(".certificado:not(.ativo)");
+
 
   function esconderBotao() {
      btnMostrarMais.classList.add("remover");
@@ -14,6 +17,15 @@
      });
  }
 
+  function esconderBotao2() {
+     btnMostrarCertificados.classList.add("remover");
+  }
+
+ function mostrarMaisCertificados() {
+    certificadosInativos.forEach(certificadoInativo => {
+        certificadoInativo.classList.add('ativo');
+  });
+}
   //Passo 2 - identificar o clique no botão
   btnMostrarMais.addEventListener('click', () => {
     //Passo 3 - adicionar a classe ativo nos projetos escondidos 
@@ -23,6 +35,14 @@
     esconderBotao();
 
   });
-  
 
 
+  //Passo 2 - identificar o clique no botão
+  btnMostrarCertificados.addEventListener('click', () => {
+    //Passo 3 - adicionar a classe ativo nos projetos escondidos 
+    mostrarMaisCertificados();
+    //- Objetivo 2 - esconder o botão de mostrar mais 
+    //  Passo 1 - pegar o botão e esconder ele 
+    esconderBotao2();
+
+  });
